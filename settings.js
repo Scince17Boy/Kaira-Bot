6522716a1f91c86765054b7db45929a84d37b4b9
Scince17Boy <tdas97041@gmail.com>
2025-10-21 18:23:08 +0530

    Initial commit

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Kaira Bot',
+  author: '‎',
+  botName: "Kaira",
+  botOwner: 'Manoj', // Your name
+  ownerNumber: '919046630534', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.2.2",
+  updateZipUrl: "https://github.com/Scince17Boy/Kaira-Bot/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
